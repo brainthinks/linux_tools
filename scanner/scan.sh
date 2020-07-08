c409e238faf3df2c0907c70179bd357e59ec22e9
Brian Andress <opensourcefiend@gmail.com>
2020-07-07 23:05:22 -0400

    add remote scanner script

--- scanner/scan.sh
+++ scanner/scan.sh
@@ -6,7 +6,7 @@
 FILENAME="${1:-scan}"
 FORMAT="${2:-"png"}"
 COMPRESSION="${3:-"None"}"
-RESOLUTION="${4:-1200}"
+RESOLUTION="${4:-75}"
 DEVICE="${5:-"hpaio:/net/HP_Color_LaserJet_MFP_M476dw?ip=192.168.1.99"}"
 
 START_TIME="$(date +%s)"
